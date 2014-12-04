8906ec166ae7237f8e05a0e6fa065a40b024aaaf
Theo Julienne <theo.julienne@gmail.com>
2014-12-04 11:38:58 -0800

    Fixes for package.sh to clean module depends since they are not correct for standalone modules.

--- kernel-module/package.sh
+++ kernel-module/package.sh
@@ -5,4 +5,7 @@ cd $(dirname $0)
 make clean
 make
 
-fpm -s dir -t deb -n gestic-driver -v 0.0.1 -C .pkg/
+# clean cruft
+rm -rf .pkg/lib/modules/*/modules.*
+
+fpm -s dir -t deb -n gestic-driver -v 0.0.1 -C .pkg/ lib
