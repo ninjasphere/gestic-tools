c1a27a3949d8c1f1e7929d04018e448e078d02b5
Theo Julienne <theo.julienne@gmail.com>
2014-12-04 12:06:48 -0800

    Add push script to optionally push the specified package.

--- kernel-module/packaging/build.sh
+++ kernel-module/packaging/build.sh
@@ -10,10 +10,14 @@ mkdir -p .tmp-pkg/lib/modules/3.12.10+/extra
 
 cp gestic.ko .tmp-pkg/lib/modules/3.12.10+/extra/gestic.ko
 
-fpm -s dir -t deb -a armhf -n gestic-driver -v 1.0.0 --after-install packaging/kernel-postinstall.sh --iteration raw -C .tmp-pkg/ lib
+PACKAGE_NAME=gestic-driver
+PACKAGE_VERSION=1.0.0
+PACKAGE_ARCH=armhf
+
+fpm -s dir -t deb -a ${PACKAGE_ARCH} -n ${PACKAGE_NAME} -v ${PACKAGE_VERSION} --after-install packaging/kernel-postinstall.sh --iteration raw -C .tmp-pkg/ lib
 
 rm -rf .tmp-pkg
 
 if [[ ! -z "$PUSH_SCRIPT" ]]; then
-	$PUSH_SCRIPT
+	$PUSH_SCRIPT $(pwd)/${PACKAGE_NAME}_${PACKAGE_VERSION}_${PACKAGE_ARCH}.deb
 fi
